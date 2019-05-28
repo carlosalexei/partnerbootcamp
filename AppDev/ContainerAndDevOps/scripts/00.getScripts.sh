1f4b560b52a7fa844b4f85e454630fd2be9abef9
Carlos Alexei Marquez <carlos.marquez@microsoft.com>
2019-05-27 21:06:42 -0700

    Update getScripts

--- AppDev/ContainerAndDevOps/scripts/00.getScripts.sh
+++ AppDev/ContainerAndDevOps/scripts/00.getScripts.sh
@@ -4,9 +4,14 @@ curl -O https://raw.githubusercontent.com/carlosalexei/partnerbootcamp/master/Ap
 curl -O https://raw.githubusercontent.com/carlosalexei/partnerbootcamp/master/AppDev/ContainerAndDevOps/scripts/02.createAzureResources.sh
 curl -O https://raw.githubusercontent.com/carlosalexei/partnerbootcamp/master/AppDev/ContainerAndDevOps/scripts/03.downloadCode.sh
 curl -O https://raw.githubusercontent.com/carlosalexei/partnerbootcamp/master/AppDev/ContainerAndDevOps/scripts/04.runMongoLocally.sh
-curl -O https://raw.githubusercontent.com/carlosalexei/partnerbootcamp/master/AppDev/ContainerAndDevOps/scripts/05.runEverythingLocally.sh
+curl -O https://raw.githubusercontent.com/carlosalexei/partnerbootcamp/master/AppDev/ContainerAndDevOps/scripts/05.buildDockerImages.sh
+curl -O https://raw.githubusercontent.com/carlosalexei/partnerbootcamp/master/AppDev/ContainerAndDevOps/scripts/06.runEverythingOnDocker.sh
+curl -O https://raw.githubusercontent.com/carlosalexei/partnerbootcamp/master/AppDev/ContainerAndDevOps/scripts/07.stopEverythingOnDocker.sh
 chmod 700 01.setupVM.sh
 chmod 700 02.createAzureResources.sh
 chmod 700 03.downloadCode.sh
 chmod 700 04.runMongoLocally.sh
-chmod 700 05.runEverythingLocally.sh
+chmod 700 05.buildDockerImages.sh
+chmod 700 06.runEverythingOnDocker.sh
+chmode700 07.stopEverythingOnDocker.sh
+
--- AppDev/ContainerAndDevOps/scripts/07.stopEverythingOnDocker.sh
+++ AppDev/ContainerAndDevOps/scripts/07.stopEverythingOnDocker.sh
@@ -1,3 +1,6 @@
 docker stop web && docker rm web
 docker stop api && docker rm api
 docker stop mongo && docker rm mongo
+
+docker rmi $(docker images -q -a)
+docker network rm fabmedical
