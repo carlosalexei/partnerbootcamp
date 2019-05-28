610083bd9a845378e478cc48eed23ce6b70d04c1
Carlos Alexei Marquez <carlos.marquez@microsoft.com>
2019-05-27 21:20:05 -0700

    Update runOnDockerCompose

--- AppDev/ContainerAndDevOps/scripts/00.getScripts.sh
+++ AppDev/ContainerAndDevOps/scripts/00.getScripts.sh
@@ -7,11 +7,13 @@ curl -O https://raw.githubusercontent.com/carlosalexei/partnerbootcamp/master/Ap
 curl -O https://raw.githubusercontent.com/carlosalexei/partnerbootcamp/master/AppDev/ContainerAndDevOps/scripts/05.buildDockerImages.sh
 curl -O https://raw.githubusercontent.com/carlosalexei/partnerbootcamp/master/AppDev/ContainerAndDevOps/scripts/06.runEverythingOnDocker.sh
 curl -O https://raw.githubusercontent.com/carlosalexei/partnerbootcamp/master/AppDev/ContainerAndDevOps/scripts/07.stopEverythingOnDocker.sh
+curl -O https://raw.githubusercontent.com/carlosalexei/partnerbootcamp/master/AppDev/ContainerAndDevOps/scripts/08.runOnDockerCompose.sh
 chmod 700 01.setupVM.sh
 chmod 700 02.createAzureResources.sh
 chmod 700 03.downloadCode.sh
 chmod 700 04.runMongoLocally.sh
 chmod 700 05.buildDockerImages.sh
 chmod 700 06.runEverythingOnDocker.sh
-chmode700 07.stopEverythingOnDocker.sh
+chmod 700 07.stopEverythingOnDocker.sh
+chmod 700 08.runOnDockerCompose.sh
 
--- AppDev/ContainerAndDevOps/scripts/08.runOnDockerCompose.sh
+++ AppDev/ContainerAndDevOps/scripts/08.runOnDockerCompose.sh
@@ -1,1 +1,8 @@
-## 
+BASE_DIR=~/FabMedical
+
+cd $BASE_DIR
+mkdir $BASE_DIR/data
+curl -O https://raw.githubusercontent.com/carlosalexei/partnerbootcamp/master/AppDev/ContainerAndDevOps/src/docker-compose.init.yml
+curl -O https://raw.githubusercontent.com/carlosalexei/partnerbootcamp/master/AppDev/ContainerAndDevOps/src/docker-compose.yml
+
+docker-compose -f docker-compose.yml -f docker-compose.init.yml -p fabmedical up -d
